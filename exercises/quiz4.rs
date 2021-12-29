d4e72ec538f21b35caf9f3e2cde7194e8dcd3df1
salva.gglez <salva.gglez@gmail.com>
2021-12-29 18:37:15 +0000

    threads y macros

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,11 +5,19 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    () => {
+        println!("Hello");
+    };
+    ($x:expr) => {
+        //println!("Hello {}", $x)
+        format!("Hello {}", $x)
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    //use super::*;
 
     #[test]
     fn test_my_macro_world() {
